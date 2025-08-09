2dd3c771f34a75aed637936df5c8d4e598baea54
esra <esrabaskaya84@gmail.com>
2025-08-09 11:55:01 +0200

    Toggle Button

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
 	<h2>Students</h2>
-	<StudentItem :data="students" name="can" />
+	<StudentItem name="Alice" age="20" mail="q1w2e@example.com" />
+	<StudentItem name="Bob" age="22" mail="KlJ5g@example.com" />
+	<StudentItem name="Charlie" age="23" mail="q1w2e@example.com" />
+	<StudentItem name="David" age="21" mail="q1w2e@example.com" />
+	<StudentItem name="Eve" age="22" mail="q1w2e@example.com" />
 </template>
 
 <script>
--- src/components/StudentItem.vue
+++ src/components/StudentItem.vue
@@ -1,21 +1,27 @@
 <template>
-	<ul v-for="student in data" :key="student.name">
-		<li>{{ student.name }}</li>
-	</ul>
-
 	{{ name }}
+	<button @click="toggleDetail()">
+		Bilgileri{{ isVisible ? "gizle" : "goster" }}
+	</button>
+	<ul v-if="isVisible">
+		<li>Age:{{ age }}</li>
+		<li>Email:{{ mail }}</li>
+	</ul>
 </template>
 
 <script>
 export default {
 	name: "StudentItem",
-	props: [
-		"name",
-		"data",
-		// You can add more props if needed, e.g., age
-	],
+	props: ["name", "age", "mail"],
 	data() {
-		return {};
+		return {
+			isVisible: false,
+		};
+	},
+	methods: {
+		toggleDetail() {
+			this.isVisible = !this.isVisible;
+		},
 	},
 };
 </script>
